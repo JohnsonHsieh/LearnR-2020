0f7898447238386f1308bcc96cde7c36081645cb
Johnson <johnson@dsp.im>
2020-10-07 18:28:41 +0800

    init

---
+++ R/init-swirl-dev.R
@@ -0,0 +1,14 @@
+local({
+  repos <- "https://wush978.github.io/R"
+  if (!require(remotes)) install.packages("remotes", repos = "http://cran.csie.ntu.edu.tw")
+  if (!require(openssl)) install.packages("openssl", repos = "http://cran.csie.ntu.edu.tw")
+  if (!require(pvm)) utils::install.packages("pvm", repos = "https://wush978.github.io/R", type = "source")
+  
+  pvm::import.packages("https://raw.githubusercontent.com/wush978/pvm-list/master/dsr.yml")
+  utils::install.packages("swirl", repos = repos)
+  library(swirl)
+  library(curl)
+  library(methods)
+  try(uninstall_course("DataScienceAndR"), silent=TRUE)
+  install_course_url(sprintf("%s/course/DataScienceAndR.zip", repos))
+})
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# LearnR-2020
+# 踏入機器學習第一步-大數據R程式語言基礎
+
+## R語言課程授課投影片
+- [Part 1. R語言的基本操作](https://johnsonhsieh.github.io/LearnR-2020/RBasic.html)
+- [Part 2. R語言的資料處理](https://johnsonhsieh.github.io/LearnR-2020/DataETL.html)
+- [Part 3. R語言的資料視覺化](https://johnsonhsieh.github.io/LearnR-2020/R_EDA_Visualization.html)
+- [Part 4. R語言的分析文件製作](https://johnsonhsieh.github.io/LearnR-2020/R_markdown.html)
+
